1a621d2d402a2e5602442679a2b910dfbf307fc0
Michael McGarry <mike.mcgarry@finbourne.com>
2021-06-15 06:03:19 +0100

    SE-1329 Removed utilities from the SDK to use the finbourne-sdk-utilities repo instead

--- sdk/lusid/__init__.py
+++ sdk/lusid/__init__.py
@@ -561,10 +561,5 @@ from lusid.models.yield_curve_data import YieldCurveData
 from lusid.models.yield_curve_data_all_of import YieldCurveDataAllOf
 
 # import utilities into sdk package
-from lusid.utilities.api_client_builder import ApiClientBuilder
-from lusid.utilities.api_configuration import ApiConfiguration
-from lusid.utilities.api_configuration_loader import ApiConfigurationLoader
-from lusid.utilities.refreshing_token import RefreshingToken
-
-# import tcp utilities
-from lusid.tcp.tcp_keep_alive_probes import TCPKeepAlivePoolManager, TCPKeepAliveProxyManager
+from lusid.utilities.api_client_factory import ApiClientFactory
+sdk/lusid/utilities/api_client_factory.py
